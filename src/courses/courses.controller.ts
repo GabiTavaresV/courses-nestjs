5dae7b41e1b282333a6b585499b779ca25344735
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-05-31 16:28:32 -0300

    feat: add get findall

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+
+@Get()
+  findall() {
+    return 'A lista de todos os cursos';
+}
+}
