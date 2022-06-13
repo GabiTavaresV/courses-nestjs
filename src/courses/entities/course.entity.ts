e456a438114fbd906d82eeef32fa8ece1d129fc2
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-06-13 19:19:58 -0300

    feat: Add TypeOrm

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Course {
-  id: number;
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column('json', { nullable: true })
   tags: string[];
 }
